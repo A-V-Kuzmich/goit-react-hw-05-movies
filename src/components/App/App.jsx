2e2be1b503badf95dc7af86b07d38d5c8b6a1fce
Dron010 <73987873+Dron010@users.noreply.github.com>
2022-01-18 17:58:54 +0200

    add file

---
+++ src/components/App/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+// import { Modal } from '../Modal';
+// import { SearchBar } from '../SearchBar';
+// import { ImageGallery } from '../ImageGallery';
+
+export default function App() {
+  // const [searchQuery, setSearchQuery] = useState('');
+  // const [showModal, setShowModal] = useState(false);
+  // const [modalImg, setModalImg] = useState({});
+  // const toggleModal = () => {
+  //   setShowModal(!showModal);
+  // };
+  // const handleClickImg = ({ target: { dataset, alt } }) => {
+  //   setModalImg({ src: dataset.src, alt: alt });
+  //   toggleModal();
+  // };
+  // return (
+  //   <>
+  //     <SearchBar Submit={setSearchQuery} />
+  //     <ImageGallery query={searchQuery} onClick={handleClickImg} />
+  //     {showModal && (
+  //       <Modal onClose={toggleModal}>
+  //         <img src={modalImg.src} alt={modalImg.alt} />
+  //       </Modal>
+  //     )}
+  //   </>
+  // );
+}
